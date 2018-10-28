90a58b8e83810a88c93882ff60deb44d2abf3e2a
Fr <abab_0101@outlook.com>
2018-10-28 21:50:43 +0800

    还是没能明白为什么这里不会重复|该用位运算而不是pow

--- 10.28_跳台阶II.cpp
+++ 10.28_跳台阶II.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int jumpFloorII(int number) {
         if(number<=0) return 0;
-        else return pow(2,number-1);
+        else return 1<<number;
+		//对噶该用位运算的
+		//else return pow(2,number-1);
     }
 };
 
