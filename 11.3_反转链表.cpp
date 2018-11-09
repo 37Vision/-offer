ffb6a2c615975f888ba5a3aff27e445d8367cc39
Fr <abab_0101@outlook.com>
2018-11-09 12:22:41 +0800

    修改为不需要申请新空间的方法

--- 11.3_反转链表.cpp
+++ 11.3_反转链表.cpp
@@ -1,4 +1,7 @@
 /**
+11.9
+不需要重新创建结点，之前是在纠结什么好像
+----------
 做得挺蠢的，不知道有没有更有趣的解法
 算法拖太久了，今天先拉一下进度，准备做vj的题目
 今晚上再来看书，总结一下前几天的内容
@@ -11,6 +14,23 @@ struct ListNode {
 			val(x), next(NULL) {
 	}
 };*/
+class Solution {
+public:
+    ListNode* ReverseList(ListNode* pHead) {
+        if(pHead==NULL) return NULL;
+        ListNode *p=pHead,*q,plast=NULL;
+        while(p!=NULL){
+            q=p->next;
+            p->next=plast;
+            plast=p;
+            p=q;
+        }
+        return plast;
+    }
+};
+
+
+
 class Solution {
 public:
     ListNode* ReverseList(ListNode* pHead) {
