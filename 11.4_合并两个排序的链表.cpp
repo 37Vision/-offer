a0d6a7a4db5eb221b78e2aa4bf1565909b3eba85
Fr <abab_0101@outlook.com>
2018-11-09 12:29:34 +0800

    添加递归算法处理形式，较为简单

--- 11.4_合并两个排序的链表.cpp
+++ 11.4_合并两个排序的链表.cpp
@@ -1,4 +1,7 @@
 /**
+11.9
+代码复杂度方面的优化可以写成一个递归的
+------
 感觉有点慢其他还好
 看看有没有更快的
 */
@@ -10,6 +13,26 @@ struct ListNode {
 			val(x), next(NULL) {
 	}
 };*/
+class Solution {
+public:
+    ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+    {
+        if(pHead1==NULL) return pHead2;
+        if(pHead2==NULL) return pHead1;
+        ListNode *p=pHead1,*q=pHead2,*newlist;
+        if(p->val<q->val){
+            newlist=p;
+            newlist->next=Merge(p->next,q);
+        }
+        else{
+            newlist=q;
+            newlist->next=Merge(p,q->next);
+        }
+        return newlist;
+    }
+};
+
+
 class Solution {
 public:
     ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
